837c116a3ff0e1e38e910d9fad35bc86e0e1cbbb
Celso Cardoso <celso.pitta@gmail.com>
2021-11-26 16:18:28 -0300

    clean hack

--- finetuning_repo/clean_files.py
+++ finetuning_repo/clean_files.py
@@ -8,13 +8,21 @@ from dataclasses import dataclass, field
 from typing import Optional
 from pathlib import Path
 import re
+import argparse
+import shutil
 
+
+parser = argparse.ArgumentParser("limpa checkpoints")
+parser.add_argument("-d","--directory",type=str, default='/data/training/gptj-qa-classifier')
+
+args = parser.parse_args()
+input_dir = args.directory
 
 PREFIX_CHECKPOINT_DIR = "checkpoint"
 _re_checkpoint = re.compile(r"^" + PREFIX_CHECKPOINT_DIR + r"\-(\d+)$")
 
 
-def get_last_checkpoint(folder):
+def get_first_checkpoint(folder):
     content = os.listdir(folder)
     checkpoints = [
         path
@@ -23,11 +31,15 @@ def get_last_checkpoint(folder):
     ]
     if len(checkpoints) == 0:
         return
-    return os.path.join(folder, max(checkpoints, key=lambda x: int(_re_checkpoint.search(x).groups()[0])))
+    
+    return os.path.join(folder, min(checkpoints, key=lambda x: int(_re_checkpoint.search(x).groups()[0]))), len(checkpoints)
 
 
+first_checkpoint, n_checkpoints = get_first_checkpoint(input_dir)
+disk_info = shutil.disk_usage(input_dir)
 
+print(f'first_checkpoint:{first_checkpoint}, number of checkpoints:{n_checkpoints}, free disk space (GB):{disk_info.free/(1000**3) }')
 
-lista = get_last_checkpoint('/data/training/gptj-qa-classifier')
-
-print(lista)
+if n_checkpoints > 2 and disk_info.free/(1000**3) < 150.0:
+    print(f'Removing checkpoint{first_checkpoint}')
+    shutil.rmtree(first_checkpoint)
